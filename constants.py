2aaba2f8bef59758cfb44a86f1277f8c7f801080
Aneesh Naik <aneesh92@hotmail.co.uk>
2019-04-16 12:14:24 +0100

    Add constants file

---
+++ constants.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created: 11th April 2019
+Author: A. P. Naik
+Description: Physical constants and unit conversions, for general use. All SI units.
+"""
+
+
+G = 6.6741e-11  # m^3 kg s^-2
+pc = 3.0857e+16  # metres
+kpc = 3.0857e+19  # metres
+M_sun = 1.9885e+30  # kg
+pi = 3.141592653589793
